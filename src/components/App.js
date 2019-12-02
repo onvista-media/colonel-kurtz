cdb0a8fe03fe63547a85d18a7499ce0af930a40c
Patrick Gollmer <patrick.gollmer@onvista.de>
2019-12-02 16:44:58 +0100

    OO-2327 : Drag & Drop

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import Animator from './Animator'
 import Blocks from '../stores/Blocks'
 import EditorBlock from './EditorBlock'
 import Switch from './Switch'
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 export default class App extends React.Component {
   getBlock(block, i) {
@@ -21,10 +23,12 @@ export default class App extends React.Component {
 
     return (
       <div className="colonel">
-        <Switch app={app} />
-        <Animator className="col-block-children">
-          {parents.map(this.getBlock, this)}
-        </Animator>
+        <DndProvider backend={HTML5Backend}>
+          <Switch app={app} />
+          <Animator className="col-block-children">
+            {parents.map(this.getBlock, this)}
+          </Animator>
+        </DndProvider>
       </div>
     )
   }
